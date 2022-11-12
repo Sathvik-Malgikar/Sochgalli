5905802cf6a6a2293c30fa8f0d4da45891b2833f
Sathvik-Malgikar <102981556+Sathvik-Malgikar@users.noreply.github.com>
2022-11-12 08:51:17 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express")
+const static = require("express-static")
+const path = require("path")
+const bodyparser = require("body-parser")
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb+srv://mongoadmin:mongoadmin@cluster0.exxx1cv.mongodb.net/sathvik?retryWrites=true&w=majority")
+
+mongoose.connection.once("open" ,()=>{
+  console.log("connected!");
+  startserver()
+}).on("error",()=>{
+  console.log("database error");
+})
+
+function startserver(){
+
+  
+  var app = express()
+  
+  
+  
+  
+  app.use ( express.static(path.join(__dirname , "static")) )
+  
+  app.use(bodyparser.urlencoded({"extended" : false}))
+  app.use(bodyparser.json())
+  
+  app.listen(542)
+  
+  app.get("/home",(req,resp)=>{
+      resp.sendFile("index.html" ,{root : "./public"})
+  })
+  app.get("/postidea",(req,resp)=>{
+    resp.sendFile("postidea.html" , {root : "./public"} )
+  })
+  
+  app.get("/browse",(req,resp)=>{
+    resp.sendFile("browse.html" , {root : "./public"} )
+  })
+  
+  app.get("/uploadidea",async (req,resp)=>{
+  console.log(req.query);
+  let data = req.query
+
+
+  let domains = data.domains.split(" ")
+
+domains.forEach(async element => {
+  
+  await mongoose.connection.collection("ideas").insertOne({ "domain" : element , "idea" : data.idea } )
+});
+
+  
+  // resp.send("your idea is submitted !")
+  resp.redirect("home")
+  })
+  
+  app.use((req,resp)=>{
+  resp.redirect("home")
+  })
+}
